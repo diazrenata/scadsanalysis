11ab56dd70517d8aebc1e469d8070dfe0dfa866b
Renata Diaz <diaz.renatam@gmail.com>
2019-11-01 14:37:20 -0400

    test add singletons

--- tests/testthat/test02_add_singletons.R
+++ tests/testthat/test02_add_singletons.R
@@ -1,26 +1,42 @@
 context("Check that adding singletons is okay")
 
-test_that("download_data", {
-  expect_true(download_data(from_url = FALSE))
+test_that("add singletons increases s0 and n0", {
+
+  dat <- load_dataset("mcdb")
+  dat <- dat %>%
+    dplyr::filter(site == "1001")
+
+  dats <- add_singletons(dat, use_max = T)
+
+  expect_true(nrow(dats) > nrow(dat))
+  expect_true(max(dats$rank) > max(dat$rank))
+
+  expect_true(sum(dats$abund) == sum(dat$abund) + (nrow(dats) - nrow(dat)))
+
 })
 
-test_that("load_dataset", {
-  datasets <- c("mcdb", "misc_abund", "bbs", "fia", "gentry")
 
-  for(i in 1:5) {
-    thisdat <- load_dataset(datasets[i])
-    expect_true(is.data.frame(thisdat))
-    expect_true(ncol(thisdat) == 7)
-    expect_true(all(
-      mode(thisdat$rank) == "numeric",
-      mode(thisdat$abund) == "numeric",
-      mode(thisdat$site) == "character",
-      mode(thisdat$dat) == "character",
-      mode(thisdat$singletons) == "logical",
-      mode(thisdat$sim) == "numeric",
-      mode(thisdat$source) == "character"
-    ))
-    expect_false(anyNA(thisdat))
-  }
+test_that("add singletons dataset agrees with individually", {
+
+  whole_dat <- load_dataset("mcdb")
+  whole_dat <- whole_dat %>%
+    dplyr::filter(site %in% c("1001", "1002", "1003"))
+
+  dat1 <- whole_dat %>%
+    dplyr::filter(site == "1001")
+  dat2 <- whole_dat %>%
+    dplyr::filter(site == "1002")
+  dat3 <- whole_dat %>%
+    dplyr::filter(site == "1003")
+
+  whole_dat_s <- add_singletons_dataset(whole_dat, use_max = T)
+
+  ind_dat_s <- dplyr::bind_rows(
+    whole_dat,
+    add_singletons(dat1, use_max = T),
+    add_singletons(dat2, use_max = T),
+  add_singletons(dat3, use_max = T))
+
+  expect_true(all(ind_dat_s == whole_dat_s))
+
 })
-
