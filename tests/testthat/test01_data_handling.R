628be0c9e90d6065c1038a707cb4653a0a57a715
Renata Diaz <diaz.renatam@gmail.com>
2019-11-01 14:05:43 -0400

    list_sites test

---
+++ tests/testthat/test01_data_handling.R
@@ -0,0 +1,39 @@
+context("Check that data handling is okay")
+
+test_that("download_data", {
+  expect_true(download_data(from_url = FALSE))
+})
+
+test_that("load_dataset", {
+  datasets <- c("mcdb", "misc_abund", "bbs", "fia", "gentry")
+
+  for(i in 1:5) {
+    thisdat <- load_dataset(datasets[i])
+    expect_true(is.data.frame(thisdat))
+    expect_true(ncol(thisdat) == 7)
+    expect_true(all(
+      mode(thisdat$rank) == "numeric",
+      mode(thisdat$abund) == "numeric",
+      mode(thisdat$site) == "character",
+      mode(thisdat$dat) == "character",
+      mode(thisdat$singletons) == "logical",
+      mode(thisdat$sim) == "numeric",
+      mode(thisdat$source) == "character"
+    ))
+    expect_false(anyNA(thisdat))
+  }
+})
+
+test_that("list_sites", {
+  datasets <- c("mcdb", "misc_abund", "bbs", "fia", "gentry")
+  for(i in 1:5) {
+    sitelist <- list_sites(datasets[i])
+    expect_true(is.data.frame(sitelist))
+    expect_true(ncol(sitelist) == 2)
+    expect_true(all(mode(sitelist$dat) == "character",
+                    mode(sitelist$site) == "character"))
+    expect_true(sitelist$dat[1] == datasets[i])
+    expect_false(anyNA(sitelist))
+  }
+}
+)
