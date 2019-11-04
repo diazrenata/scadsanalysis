94b7fe249dde1f2aa8d56170db1d1e16ce2ecf84
Renata Diaz <diaz.renatam@gmail.com>
2019-11-04 14:46:33 -0500

    test sample_fs

--- tests/testthat/test04_sample_fs.R
+++ tests/testthat/test04_sample_fs.R
@@ -1,43 +1,36 @@
-context("Check statevars")
+context("Check FS sampling functions")
 
-test_that("get statevars", {
+test_that("sampling fs works", {
 
   dat <- load_dataset("mcdb")
   dat <- dat %>%
-    dplyr::filter(site %in% c("1001", "1002"))
+    dplyr::filter(site == "1001")
 
-  dats <- add_singletons_dataset(dat, use_max = T)
+  sv <- get_statevars(dat)
 
-  sv <- get_statevars(dats)
+  set.seed(1)
+  fs_samples <- sample_fs_wrapper(dat, site_name = "1001", singletonsyn = F, n_samples = 2, p_table = NULL)
 
-  expect_true(ncol(sv) == 7)
+  expect_true(ncol(fs_samples) == 7)
   expect_true(all(
-    mode(sv$site) == "character",
-    mode(sv$dat) == "character",
-    mode(sv$singletons) == "logical",
-    mode(sv$sim) == "numeric",
-    mode(sv$source) == "character",
-    mode(sv$s0) == "numeric",
-    mode(sv$n0) == "numeric"
+    mode(fs_samples$site) == "character",
+    mode(fs_samples$dat) == "character",
+    mode(fs_samples$singletons) == "logical",
+    mode(fs_samples$sim) == "numeric",
+    mode(fs_samples$source) == "character",
+    mode(fs_samples$rank) == "numeric",
+    mode(fs_samples$abund) == "numeric"
   ))
-  expect_false(anyNA(sv))
+  expect_false(anyNA(fs_samples))
 
-  expect_true(
-    dplyr::filter(sv, site == "1001", singletons == FALSE)$n0 == sum(dplyr::filter(dats, site == "1001", singletons == FALSE)$abund)
-  )
+  expect_true(min(fs_samples$abund) > 0)
 
-  expect_true(
-    dplyr::filter(sv, site == "1001", singletons == FALSE)$s0 == nrow(dplyr::filter(dats, site == "1001", singletons == FALSE))
-  )
+  fs_samples_sv <- fs_samples %>%
+    dplyr::group_by(sim) %>%
+    dplyr::summarize(s0 = dplyr::n(), n0 = sum(abund)) %>%
+    dplyr::ungroup()
 
-  expect_true(
-    dplyr::filter(sv, site == "1001", singletons == TRUE)$n0 == sum(dplyr::filter(dats, site == "1001", singletons == TRUE)$abund)
-  )
-
-  expect_true(
-    dplyr::filter(sv, site == "1001", singletons == TRUE)$s0 == nrow(dplyr::filter(dats, site == "1001", singletons == TRUE))
-  )
-
-  expect_true(nrow(dplyr::filter(dats, site == "1001", singletons == TRUE)) > nrow(dplyr::filter(dats, site == "1001", singletons == FALSE)))
+  expect_true(all(fs_samples_sv$n0 == sv$n0))
+  expect_true(all(fs_samples_sv$s0 == sv$s0))
 
 })
