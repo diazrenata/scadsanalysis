922ab2d8103529bf9cbd69d666d46c9be750ee0e
Renata Diaz <diaz.renatam@gmail.com>
2019-11-07 17:29:04 -0500

    run this

--- analysis/dat_pipeline.R
+++ analysis/dat_pipeline.R
@@ -6,14 +6,12 @@ expose_imports("scadsanalysis")
 
 # datasets <- c("mcdb", "gentry", "bbs", "misc_abund", "fia")
 
-#datasets <- c("mcdb", "gentry", "bbs", "misc_abund")
-
-datasets <- c("mcdb")
+datasets <- c("mcdb", "bbs")
 
 sites_list <- lapply(as.list(datasets), FUN = list_sites)
 names(sites_list) <- datasets
 #sites_list$mcdb <- sites_list$mcdb[1:10, ]
-ndraws = 10000
+ndraws = 100
 
 dat_plan <- drake_plan(
   dat = target(load_dataset(dataset_name = d),
@@ -36,14 +34,32 @@ dat_plan <- drake_plan(
                      hpc = F),
   mamm_p = target(readRDS(here::here("analysis", "masterp_mamm.Rds")),
                   hpc = F),
+  wide_p = target(readRDS(here::here("analysis", "masterp_wide.Rds")),
+                  hpc = F),
   fs_mcdb = target(sample_fs_wrapper(dataset = dat_s_dat_mcdb, site_name = s, singletonsyn = singletons, n_samples = ndraws, p_table = mamm_p),
                    transform = cross(s = !!sites_list$mcdb$site,
                                      singletons = !!c(TRUE, FALSE))),
-  di = target(add_dis(fs_mcdb),
+  # fs_fia = target(sample_fs_wrapper(dataset = dat_s_dat_fia, site_name = s, singletonsyn = singletons, n_samples = ndraws, p_table = wide_p),
+  #                 transform = cross(s = !!sites_list$fia$site,
+  #                                   singletons = !!c(TRUE, FALSE))),
+  # fs_gentry = target(sample_fs_wrapper(dataset = dat_s_dat_gentry, site_name = s, singletonsyn = singletons, n_samples = ndraws, p_table = wide_p),
+  #                 transform = cross(s = !!sites_list$gentry$site,
+  #                                   singletons = !!c(TRUE, FALSE))),
+  fs_bbs = target(sample_fs_wrapper(dataset = dat_s_dat_bbs, site_name = s, singletonsyn = singletons, n_samples = ndraws, p_table = wide_p),
+                  transform = cross(s = !!sites_list$bbs$site,
+                                    singletons = !!c(TRUE, FALSE))),
+  mcdb_di = target(add_dis(fs_mcdb),
               transform = map(fs_mcdb)),
-  all_di = target(dplyr::bind_rows(di),
-                    transform = combine(di),
-                    hpc = F)
+  bbs_di = target(add_dis(fs_bbs),
+              transform = map(fs_bbs)),
+  all_mcdb_di = target(dplyr::bind_rows(mcdb_di),
+                    transform = combine(mcdb_di),
+                    hpc = F),
+  all_bbs_di = target(dplyr::bind_rows(bbs_di),
+                      transform = combine(bbs_di),
+                      hpc = F),
+  all_di = target(dplyr::bind_rows(all_mcdb_di, all_bbs_di),
+                  hpc = F)
 )
 
 all <- dat_plan
@@ -73,7 +89,7 @@ if(grepl("ufhpc", nodename)) {
        cache_log_file = here::here("analysis", "drake", "cache_log_dat.txt"),
        verbose = 2,
        parallelism = "clustermq",
-       jobs = 10,
+       jobs = 20,
        caching = "master") # Important for DBI caches!
 } else {
   library(clustermq)
