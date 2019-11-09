9b88ea65521de5b31f38076612178dcc32dd490c
Renata Diaz <diaz.renatam@gmail.com>
2019-11-09 11:10:47 -0500

    fix miscabund path, put worker logs in a subdir

--- analysis/miscabund_pipeline.R
+++ analysis/miscabund_pipeline.R
@@ -53,7 +53,7 @@ nodename <- Sys.info()["nodename"]
 if(grepl("ufhpc", nodename)) {
   print("I know I am on the HiPerGator!")
   library(clustermq)
-  options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermql.tmpl")
+  options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmpl")
   ## Run the pipeline parallelized for HiPerGator
   make(all,
        force = TRUE,
