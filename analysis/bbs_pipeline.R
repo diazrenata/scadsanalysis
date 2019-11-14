3035c0105b2abfe3482f04e12dfd2942375f9c92
Renata Diaz <diaz.renatam@gmail.com>
2019-11-13 21:33:22 -0500

    fix bbs?

--- analysis/bbs_pipeline.R
+++ analysis/bbs_pipeline.R
@@ -7,8 +7,8 @@ expose_imports("scadsanalysis")
 datasets <- "bbs"
 
 sites_list <- list_sites("bbs")
-ndraws = 100
-sites_list <- sites_list[1:15, ]
+ndraws = 2500
+#sites_list <- sites_list[1:15, ]
 dat_plan <- drake_plan(
   dat = target(load_dataset(dataset_name = d),
                transform = map(
@@ -26,9 +26,10 @@ dat_plan <- drake_plan(
                                      singletons = !!c(TRUE, FALSE))),
   di = target(add_dis(fs),
               transform = map(fs)),
-  all_di = target(dplyr::bind_rows(di),
-                  transform = combine(di),
+  all_di_s = target(dplyr::bind_rows(di),
+                  transform = combine(di, .by = singletons),
                   hpc = F),
+  all_di = target(dplyr::bind_rows(all_di_s_TRUE, all_di_s_FALSE), hpc = F),
   report = target(render_report(here::here("analysis", "reports", "dat_report_template.Rmd"), dependencies = all_di, is_template = TRUE, dat_name = !!datasets),
                   trigger = trigger(condition = T),
                   hpc = F)
