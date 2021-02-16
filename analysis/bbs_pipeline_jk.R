3a20a622e6f3f33d744df666403cd8f84dc18bf1
Renata Diaz <diaz.renatam@gmail.com>
2021-02-15 22:06:59 -0500

    run som stuffs

---
+++ analysis/bbs_pipeline_jk.R
@@ -0,0 +1,86 @@
+library(drake)
+library(scadsanalysis)
+library(ggplot2)
+library(dplyr)
+
+expose_imports("scadsanalysis")
+
+dataset <- read.csv(here::here("analysis", "rev_prototyping", "jacknifed_datasets", "bbs_jk.csv"))
+
+max_n_sites = 1000
+
+sites_list <- dataset %>%
+  select(site, dat) %>%
+  distinct() %>%
+  group_by_all() %>%
+  mutate(site_source = unlist(strsplit(site, "_")[[1]][[1]])) %>%
+  ungroup()
+
+sites_list <- sites_list %>%
+  filter(site_source %in% unique(sites_list$site_source)[1:max_n_sites]) %>%
+  select(-site_source)
+
+ndraws = 4000
+
+
+#sites_list <- sites_list[1:5, ]
+set.seed(1980)
+
+
+all <- drake_plan(
+  dat = target(read.csv(here::here("analysis", "rev_prototyping", "jacknifed_datasets", "bbs_jk.csv")),
+               hpc = F
+  ),
+  wide_p = target(readRDS(here::here("analysis", "masterp_wide.Rds")),
+                  hpc = F),
+  fs = target(sample_fs_wrapper(dataset = dat, site_name = s, singletonsyn = FALSE, n_samples = ndraws, p_table = wide_p, seed = !!sample.int(10^6, size = 1)),
+              transform = map(s = !!sites_list$site)),
+  fs_diffs = target(get_fs_diffs(fs),
+                    transform = map(fs)),
+  di = target(add_dis(fs, fs_diffs),
+              transform = map(fs, fs_diffs)),
+  di_obs = target(pull_di(di),
+                  transform = map(di)),
+  all_di_obs = target(dplyr::bind_rows(di_obs),
+                    transform = combine(di_obs))
+)
+
+## Set up the cache and config
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("analysis", "drake", "drake-cache-bbs-jk.sqlite"))
+cache <- storr::storr_dbi("datatable", "keystable", db)
+cache$del(key = "lock", namespace = "session")
+#
+# ## View the graph of the plan
+# if (interactive())
+# {
+#   config <- drake_config(all, cache = cache)
+#   sankey_drake_graph(config, build_times = "none")  # requires "networkD3" package
+#   vis_drake_graph(config, build_times = "none")     # requires "visNetwork" package
+# }
+#
+## Run the pipeline
+nodename <- Sys.info()["nodename"]
+if(grepl("ufhpc", nodename)) {
+  print("I know I am on the HiPerGator!")
+  library(clustermq)
+  options(clustermq.scheduler = "slurm", clustermq.template = here::here("slurm_clustermq.tmpl"))
+  ## Run the pipeline parallelized for HiPerGator
+  make(all,
+       force = TRUE,
+       cache = cache,
+       cache_log_file = here::here("analysis", "drake", "cache_log_bbs_jk.txt"),
+       verbose = 2,
+       parallelism = "clustermq",
+       jobs = 20,
+       caching = "main") # Important for DBI caches!
+} else {
+  # library(clustermq)
+  # options(clustermq.scheduler = "multicore")
+  # Run the pipeline on multiple local cores
+  system.time(make(all, cache = cache, cache_log_file = here::here("analysis", "drake", "cache_log_bbs_jk.txt")))
+}
+
+DBI::dbDisconnect(db)
+rm(cache)
+
+print("Completed OK")
--- analysis/rev_prototyping/jackknife_mcdb_results.md
+++ analysis/rev_prototyping/jackknife_mcdb_results.md
@@ -15,7 +15,41 @@ Renata Diaz
 
     ## Joining, by = c("dat", "site_source")
 
-![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-2.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-3.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-4.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-5.png)<!-- -->
+    ## `stat_bin()` using `bins = 30`. Pick better value with `binwidth`.
+
+![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->
+
+    ## `stat_bin()` using `bins = 30`. Pick better value with `binwidth`.
+
+![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-2.png)<!-- -->
+
+    ## `stat_bin()` using `bins = 30`. Pick better value with `binwidth`.
+
+![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-3.png)<!-- -->
+
+    ## [1] 0.1726784
+
+    ## [1] 0.1189563
+
+    ## [1] 0.4098235
+
+    ## [1] 0.2977744
+
+    ## [1] 0.3737529
+
+    ## [1] 0.2547966
+
+<div class="kable-table">
+
+| even\_change         |   n |
+| :------------------- | --: |
+| actualFALSE\_jkFALSE | 816 |
+| actualTRUE\_jkFALSE  | 155 |
+| actualTRUE\_jkTRUE   | 332 |
+
+</div>
+
+![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-4.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-5.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-6.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-7.png)<!-- -->![](jackknife_mcdb_results_files/figure-gfm/unnamed-chunk-3-8.png)<!-- -->
 
 So for these 50 communities, resampling does not change the outcome for
 the 6-10 largest communities. It changes it for a handful of smaller
